f363db9d3ea93e2969eb567f019b26a6b4ce7c6a
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-10-20 12:32:14 +0200

    Create R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs

---
+++ R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs
@@ -0,0 +1,16 @@
+// https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka3/dopuna_do_punog_kvadrata
+// https://petlja.org/sr-Latn-RS/biblioteka/r/problemi/Zbirka-stara/dopuna_do_punog_kvadrata
+// https://arena.petlja.org/sr-Latn-RS/competition/r1-t05-deljivost-prosti-tle-001-2024#tab_132802
+// https://petlja.org/sr-Latn-RS/kurs/14606/23/2756
+// 
+
+
+using System;
+
+class R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata
+{
+    static void Main()
+    {
+        Console.WriteLine();
+    }
+}
