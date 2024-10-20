080a0f7ff85b14ab75ea1a6bdfa892b509eeac1a
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-10-20 12:58:01 +0200

    Update R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs

--- R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs
+++ R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs
@@ -2,7 +2,7 @@
 // https://petlja.org/sr-Latn-RS/biblioteka/r/problemi/Zbirka-stara/dopuna_do_punog_kvadrata
 // https://arena.petlja.org/sr-Latn-RS/competition/r1-t05-deljivost-prosti-tle-001-2024#tab_132802
 // https://petlja.org/sr-Latn-RS/kurs/14606/23/2756
-// 
+// https://github.com/draganilicnis/R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata/blob/main/R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata.cs
 
 
 using System;
@@ -11,6 +11,24 @@ class R1_T05_05_Deljivost_Prost_TLE_Dopuna_do_punog_kvadrata
 {
     static void Main()
     {
-        Console.WriteLine();
+        long n = long.Parse(Console.ReadLine());
+
+        long m = 1;
+
+        long d = 2;  
+        while (d * d <= n)
+        {
+            int k = 0;
+            while (n % d == 0)
+            {
+                n = n / d;
+                k++;
+            }
+            if (k % 2 != 0) m = m * d;
+            d++;
+        }
+        if (n > 1) m = m * n;
+        
+        Console.WriteLine(m);
     }
 }
